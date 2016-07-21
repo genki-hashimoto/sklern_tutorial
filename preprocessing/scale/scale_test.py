18ff4f128ae1985c74d1bb7399187a6793127ef2
Genki Hashimoto <knct.gen@gmail.com>
2016-07-21 09:49:55 +0900

    scale追加

--- preprocessing/scale/scale_test.py
+++ preprocessing/scale/scale_test.py
@@ -1,2 +1,28 @@
-from sklearn import preprocessing
+from sklearn.preprocessing import scale
 import numpy as np
+import pandas as pd
+
+
+dim1_nparray = np.array([1,2,3,4,5])
+dim1_df = pd.Series([1,2,3,4,5])
+dim1_array = [1,2,3,4,5]
+
+s_dim1_nparray = scale(dim1_nparray)
+s_dim1_array = scale(dim1_array)
+s_dim1_df = scale(dim1_df)
+print("array")
+print(dim1_array)
+print(s_dim1_array)
+print(s_dim1_array.mean(), s_dim1_array.var())
+print(dim1_nparray)
+print(s_dim1_nparray)
+print(s_dim1_nparray.mean(), s_dim1_nparray.var())
+print(dim1_df)
+print(s_dim1_df)
+print(s_dim1_df.mean(), s_dim1_df.var())
+
+dim2_array = [[1,2],[2,3],[3,4],[4,5],[5,6]]
+s_dim2_array = scale(dim2_array)
+print(dim2_array)
+print(s_dim2_array)
+print(s_dim2_array.mean(),s_dim2_array.var())
