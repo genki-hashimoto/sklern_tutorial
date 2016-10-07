02a83863dca2d0a42a3b93d73d1368a9b59e1cef
Genki Hashimoto <knct.gen@gmail.com>
2016-10-07 14:16:28 +0900

    svm追加

---
+++ svm/README.md
@@ -0,0 +1,9 @@
+# svm
+
+## LinerSVC
+線形なサポートベクターマシン．サンプルではIRIS Datasetsのpetal length,petal widthの二つを抜き取り分類する．
+Cを変更することで許容する分類不可なものの侵入距離が変わる
+
+## SVC
+様々なサポートベクターマシン．サンプルではIRIS Datasetsのpetal length,petal widthの二つを抜き取り分類する．
+SVCクラスのkernel変数を変更することで使用するカーネル関数を変えられる（線形もある)．Cを変更することで許容する分類不可なものの侵入距離が変わる
---
+++ svm/SVC.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn import datasets
+import pandas as pd
+
+#irisデータのロード
+iris = datasets.load_iris()
+#必要な特徴を抜き出し
+features = np.array(pd.DataFrame(iris.data,columns = iris.feature_names).ix[:,2:])
+#ラベル(教師信号)の抜き出し
+target = iris.target
+
+#SVMの学習
+clf = svm.SVC(C=1)
+clf.fit(features,target)
+
+#トレーニングデータの描画
+plt.scatter(features[target==0][:,0],features[target==0][:,1],color="red")
+plt.scatter(features[target==1][:,0],features[target==1][:,1],color="blue")
+plt.scatter(features[target==2][:,0],features[target==2][:,1],color="green")
+
+#データ範囲にメッシュ状にデータ点を生成
+x_min = features[:,0].min()-1
+x_max = features[:,0].max()+1
+y_min = features[:,1].min()-1
+y_max = features[:,1].max()+1
+grid_interval=0.02
+xx, yy = np.meshgrid(
+	np.arange(x_min, x_max, grid_interval),
+	np.arange(y_min, y_max, grid_interval),
+)
+
+#メッシュ状のデータ点を学習済みSVMで分類
+Z = clf.predict(np.c_[xx.ravel(),yy.ravel()])
+
+# 分類結果を表示する
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, cmap=plt.cm.bone, alpha=0.2)
+
+# グラフを表示する
+plt.autoscale()
+plt.grid()
+plt.show()
